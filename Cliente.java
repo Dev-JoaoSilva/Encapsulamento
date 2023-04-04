b351f06b6a4d6cc0ae9e24391db6841a847bf1fe
João Gabriel <joao.silva2714@etec.sp.gov.br>
2023-04-04 13:45:56 +0000

    Salvar

---
+++ Cliente.java
@@ -0,0 +1,14 @@
+public class Cliente {
+    
+    public static void main(String[] args) {
+        
+        Conta conta = new Conta();
+        conta.nome="Leonardo";
+        conta.salario=10000;
+
+        System.out.println(conta.nome);
+        System.out.println(conta.salario);
+
+    }
+
+}
